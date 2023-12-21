a805cf01a974844aac7ce5476e523d9b74cbd099
Hyper <133499771+ItzMeHyper@users.noreply.github.com>
2023-12-22 00:22:18 +0530

    Update ping.js

--- ping.js
+++ ping.js
@@ -11,8 +11,6 @@ module.exports = {
  * @param {Client} client 
  */
     async execute(interaction, client) {
-
-        try {
             const ping = interaction.CreatedTimestamp - interaction.CreatedTimestamp
             const pingEmbed = new EmbedBuilder()
                 .setColor(0x008000)
@@ -26,25 +24,5 @@ module.exports = {
 
             interaction.reply({ content: "✅ - Well this is the current ping!", embeds: [pingEmbed] })
 
-        } catch (err) {
-            const stack = err.stack.split('\n')[1].trim();
-            const stackTrace = err.stack;
-            const regex = /\((\S+)\)/;
-            const match = regex.exec(stackTrace);
-            const lineNumber = match ? match[1] : "unknown";
-            const errEmbed = new EmbedBuilder()
-                .setColor("Red")
-                .setTitle("⚠️ Error Occurred ⚠️")
-                .addFields([
-                    { name: '🖥️ **ERROR:**', value: `\`\`\`md\n${err}\`\`\``, inline: false },
-                    { name: '🖥️ **ERR:**', value: `\`\`\`md\n${err.message}\`\`\``, inline: true },
-                    { name: '🖥️ **Event no:**', value: `\`\`\`md\n${lineNumber}\`\`\``, inline: true },
-                    { name: '👤 **Triggered By:**', value: `\`\`\`md\n${interaction.user.username} #${interaction.user.discriminator}\`\`\``, inline: true },
-                    { name: '🖥️ **Path:**', value: `\`\`\`${stack}\`\`\``, inline: true },
-                ])
-                .setFooter({ text: `Command -- ${interaction.commandName}` })
-                .setTimestamp();
-            client.channels.cache.get(process.env.ERRORCHANNEL_ID).send({ embeds: [errEmbed] })
-        }
     },
 };
