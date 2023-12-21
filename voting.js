cb1a42103e3a69fd9e8a99c2d0b578e791323c10
Hyper <133499771+ItzMeHyper@users.noreply.github.com>
2023-12-22 00:28:49 +0530

    Update voting.js

--- voting.js
+++ voting.js
@@ -69,7 +69,6 @@ module.exports = {
 		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
 	async execute(interaction, client) {
-		try {
 			const topic = interaction.options.getString("topic")
 			const whatever = interaction.options.getString("message")
 			const destination =
@@ -123,25 +122,5 @@ module.exports = {
 			setTimeout(() => {
 				interaction.deleteReply();
 			}, 5000);
-		} catch (err) {
-			const stack = err.stack.split('\n')[1].trim();
-			const stackTrace = err.stack;
-			const regex = /\((\S+)\)/;
-			const match = regex.exec(stackTrace);
-			const lineNumber = match ? match[1] : "unknown";
-			const errEmbed = new EmbedBuilder()
-				.setColor("Red")
-				.setTitle("⚠️ Error Occurred ⚠️")
-				.addFields([
-					{ name: '🖥️ **ERROR:**', value: `\`\`\`md\n${err}\`\`\``, inline: false },
-					{ name: '🖥️ **ERR:**', value: `\`\`\`md\n${err.message}\`\`\``, inline: true },
-					{ name: '🖥️ **Event no:**', value: `\`\`\`md\n${lineNumber}\`\`\``, inline: true },
-					{ name: '👤 **Triggered By:**', value: `\`\`\`md\n${interaction.user.username} #${interaction.user.discriminator}\`\`\``, inline: true },
-					{ name: '🖥️ **Path:**', value: `\`\`\`${stack}\`\`\``, inline: true },
-				])
-				.setFooter({ text: `Command -- ${interaction.commandName}` })
-				.setTimestamp();
-			client.channels.cache.get(process.env.ERRORCHANNEL_ID).send({ embeds: [errEmbed] })
-		}
 	},
 };
